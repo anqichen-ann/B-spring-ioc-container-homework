e95aadb8645cb52f1563cf719400d96d521ebe09
annie <anqi.chen@thoughtworks.com>
2020-10-23 20:12:26 +0800

    feat p2s1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,18 +1,28 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PreDestroy;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
 @RestController
+@Scope(SCOPE_PROTOTYPE)
 public class GreetingController {
 
-    private final GreetingService greetingService;
+    private  final GreetingService greetingService;
 
     @Autowired
     public GreetingController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
+
 
     @GetMapping("/greet")
     public String greet() {
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
 @Service
+@Scope(SCOPE_PROTOTYPE)
 public class GreetingService {
+
+    public GreetingService(){
+        System.out.println("Greetingservice------------");
+    }
 
     String sayHi() {
         return "hello world";
